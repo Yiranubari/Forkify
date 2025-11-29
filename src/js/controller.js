8b1506b8c34b51238bc1505c9e184a684e9d84d5
Yiranubari <yiranubari4@gmail.com>
2025-11-29 22:27:23 +0100

    feat: Update showRecipe function to dynamically render recipe details and handle API data

--- src/js/controller.js
+++ src/js/controller.js
@@ -44,7 +44,7 @@ const showRecipe = async function () {
         <figure class="recipe__fig">
           <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
           <h1 class="recipe__title">
-            <span>Pasta with tomato cream sauce</span>
+            <span>${recipe.title}</span>
           </h1>
         </figure>
 
@@ -53,14 +53,14 @@ const showRecipe = async function () {
             <svg class="recipe__info-icon">
               <use href="src/img/icons.svg#icon-clock"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">45</span>
+            <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
             <span class="recipe__info-text">minutes</span>
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
               <use href="src/img/icons.svg#icon-users"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--people">4</span>
+            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
             <span class="recipe__info-text">servings</span>
 
             <div class="recipe__info-buttons">
@@ -120,12 +120,12 @@ const showRecipe = async function () {
           <h2 class="heading--2">How to cook it</h2>
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
-            <span class="recipe__publisher">The Pioneer Woman</span>. Please check out
+            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
             directions at their website.
           </p>
           <a
             class="btn--small recipe__btn"
-            href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
+            href="${recipe.sourceUrl}"
             target="_blank"
           >
             <span>Directions</span>
@@ -134,6 +134,7 @@ const showRecipe = async function () {
             </svg>
           </a>
         </div>`;
+    recipeContainer.insertAdjacentElement('afterbegin', markup);
   } catch (err) {
     alert(err);
   }
