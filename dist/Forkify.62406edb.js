730e84535f19f9b4261f8590174c89ecad643758
Yiranubari <yiranubari4@gmail.com>
2025-11-29 22:08:13 +0100

    feat: Update showRecipe function to handle API errors and modify recipe ID

--- dist/Forkify.62406edb.js
+++ dist/Forkify.62406edb.js
@@ -11,8 +11,9 @@ const timeout = function(s) {
 ///////////////////////////////////////
 const showRecipe = async function() {
     try {
-        const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
+        const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886zzzzz');
         const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
         console.log(res, data);
     } catch (err) {
         alert(err);
