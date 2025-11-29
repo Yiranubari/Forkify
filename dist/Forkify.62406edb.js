41ff6a6cf730a74973e2e85a25df5be8d9e5747c
Yiranubari <yiranubari4@gmail.com>
2025-11-29 22:10:35 +0100

    Refactor code structure for improved readability and maintainability

--- dist/Forkify.62406edb.js
+++ dist/Forkify.62406edb.js
@@ -11,10 +11,11 @@ const timeout = function(s) {
 ///////////////////////////////////////
 const showRecipe = async function() {
     try {
-        const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886zzzzz');
+        const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
         const data = await res.json();
         if (!res.ok) throw new Error(`${data.message} (${res.status})`);
         console.log(res, data);
+        let recipe = data.data.recipe;
     } catch (err) {
         alert(err);
     }
--- src/js/controller.js
+++ src/js/controller.js
@@ -16,13 +16,14 @@ const timeout = function (s) {
 const showRecipe = async function () {
   try {
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886zzzzz'
+      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
 
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
     console.log(res, data);
+    let recipe = data.data.recipe;
   } catch (err) {
     alert(err);
   }
