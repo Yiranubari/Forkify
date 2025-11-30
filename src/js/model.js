cc9348ce25d6306534a9fc837d2c5d36ad088110
Yiranubari <yiranubari4@gmail.com>
2025-11-30 18:07:58 +0100

    Refactor code for model.js

---
+++ src/js/model.js
@@ -0,0 +1,27 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function () {
+  const res = await fetch(
+    // 'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8673'
+    `https://forkify-api.jonas.io/api/v2/recipes/${id}`
+  );
+
+  const data = await res.json();
+  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+  console.log(res, data);
+  let { recipe } = data.data;
+  recipe = {
+    id: recipe.id,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.source_url,
+    image: recipe.image_url,
+    servings: recipe.servings,
+    cookingTime: recipe.cooking_time,
+    ingredients: recipe.ingredients,
+  };
+  console.log(recipe);
+};
