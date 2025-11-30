7d76f3a1524a22084798b0a0db0d6863295ad58a
Yiranubari <yiranubari4@gmail.com>
2025-11-30 20:18:47 +0100

    Refactor RecipeView to use class property for data access
    - Updated the #generateMarkup method to reference this.#data instead of recipe object directly.
    - Ensured all instances of recipe properties (image, title, cookingTime, servings, ingredients, publisher, sourceUrl) are replaced with this.#data for consistency and encapsulation.

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -8,11 +8,11 @@ class RecipeView {
   #generateMarkup() {
     return `
         <figure class="recipe__fig">
-          <img src="${recipe.image}" alt="${
-      recipe.title
+          <img src="${this.#data.image}" alt="${
+      this.#data.title
     }" class="recipe__img" />
           <h1 class="recipe__title">
-            <span>${recipe.title}</span>
+            <span>${this.#data.title}</span>
           </h1>
         </figure>
 
@@ -22,7 +22,7 @@ class RecipeView {
               <use href="${icons}#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${
-              recipe.cookingTime
+              this.#data.cookingTime
             }</span>
             <span class="recipe__info-text">minutes</span>
           </div>
@@ -31,7 +31,7 @@ class RecipeView {
               <use href="${icons}#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${
-              recipe.servings
+              this.#data.servings
             }</span>
             <span class="recipe__info-text">servings</span>
 
@@ -64,7 +64,7 @@ class RecipeView {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-            ${recipe.ingredients
+            ${this.#data.ingredients
               .map(ing => {
                 return `
                 
@@ -90,13 +90,13 @@ class RecipeView {
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
             <span class="recipe__publisher">${
-              recipe.publisher
+              this.#data.publisher
             }</span>. Please check out
             directions at their website.
           </p>
           <a
             class="btn--small recipe__btn"
-            href="${recipe.sourceUrl}"
+            href="${this.#data.sourceUrl}"
             target="_blank"
           >
             <span>Directions</span>
