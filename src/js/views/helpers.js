3e70e561926e637150de5f96cfc169b5b2429d4b
Yiranubari <yiranubari4@gmail.com>
2025-12-06 14:05:51 +0100

    fix: wrap fetch logic in try-catch for better error handling in getJSON function

--- src/js/views/helpers.js
+++ src/js/views/helpers.js
@@ -1,7 +1,11 @@
 export const getJSON = async function (url) {
-  const res = await fetch(url);
-  const data = await res.json();
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
 
-  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-  return data;
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
