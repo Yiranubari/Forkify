fe1f0ff2a63ddb19ab06b3e8a5f4977db1562278
Yiranubari <yiranubari4@gmail.com>
2025-12-06 14:04:49 +0100

    fix: update getJSON function to use dynamic URL parameter

--- src/js/views/helpers.js
+++ src/js/views/helpers.js
@@ -1,8 +1,7 @@
 export const getJSON = async function (url) {
-  const res = await fetch(
-    // 'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8673'
-    `${API_URL}/${id}`
-  );
+  const res = await fetch(url);
   const data = await res.json();
+
   if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+  return data;
 };
