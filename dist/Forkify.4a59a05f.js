98bb8b3a9a2c01547c8e6d779efb26c2a2b88959
Yiranubari <yiranubari4@gmail.com>
2025-12-07 17:21:30 +0100

    Update fetch timeout duration in getJSON function

--- dist/Forkify.4a59a05f.js
+++ dist/Forkify.4a59a05f.js
@@ -2669,7 +2669,7 @@ const getJSON = async function(url) {
         const fetchPro = fetch(url);
         const res = await Promise.race([
             fetchPro,
-            timeout(50)
+            timeout((0, _configJs.TIMEOUT_SEC))
         ]);
         const data = await res.json();
         if (!res.ok) throw new Error(`${data.message} (${res.status})`);
