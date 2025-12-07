4bb4af529c6440e82b457f76e81be0685dd311d9
Yiranubari <yiranubari4@gmail.com>
2025-12-07 17:01:33 +0100

    Refactor error handling in getJSON function to throw errors instead of logging them

--- dist/Forkify.4a59a05f.js
+++ dist/Forkify.4a59a05f.js
@@ -2667,7 +2667,7 @@ const getJSON = async function(url) {
         if (!res.ok) throw new Error(`${data.message} (${res.status})`);
         return data;
     } catch (err) {
-        console.error(err);
+        throw err;
     }
 };
 
