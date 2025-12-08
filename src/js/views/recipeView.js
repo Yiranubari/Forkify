a732e96c2e50bbe2102e68eab1ed250fc08a17a6
Yiranubari <yiranubari4@gmail.com>
2025-12-08 08:18:00 +0100

    feat: add renderError method to display error message when no recipes are found

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -28,6 +28,18 @@ class RecipeView {
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   };
 
+  renderError() {
+    const markup = `
+  <div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>No recipes found for your query. Please try again!</p>
+          </div>`;
+  }
+
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
   }
