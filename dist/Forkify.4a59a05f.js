de7d73d6b073c57d4b6a12898a06ea5d538a89cf
Yiranubari <yiranubari4@gmail.com>
2025-12-09 07:06:04 +0100

    Add markup generation for results preview in ResultsView

--- dist/Forkify.4a59a05f.js
+++ dist/Forkify.4a59a05f.js
@@ -3179,6 +3179,26 @@ var _viewJs = require("./view.js");
 var _viewJsDefault = parcelHelpers.interopDefault(_viewJs);
 class ResultsView extends (0, _viewJsDefault.default) {
     _parentElement = document.querySelector('.results');
+    _generateMarkup() {
+        return `
+    <li class="preview">
+            <a class="preview__link preview__link--active" href="#23456">
+              <figure class="preview__fig">
+                <img src="src/img/test-1.jpg" alt="Test" />
+              </figure>
+              <div class="preview__data">
+                <h4 class="preview__title">Pasta with Tomato Cream ...</h4>
+                <p class="preview__publisher">The Pioneer Woman</p>
+                <div class="preview__user-generated">
+                  <svg>
+                    <use href="src/img/icons.svg#icon-user"></use>
+                  </svg>
+                </div>
+              </div>
+            </a>
+          </li>
+          `;
+    }
 }
 exports.default = new ResultsView();
 
