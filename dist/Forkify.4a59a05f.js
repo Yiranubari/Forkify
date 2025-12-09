c541d615c5fcf1b5cd8f13e1d9dd6e60742cdecb
Yiranubari <yiranubari4@gmail.com>
2025-12-09 09:50:30 +0100

    Refactor ResultsView markup generation
    - Cleaned up the _generateMarkup method for better readability.
    - Removed the active class from the preview link.
    - Simplified the markup in _generateMarkupPreview by removing the user-generated content section.

--- dist/Forkify.4a59a05f.js
+++ dist/Forkify.4a59a05f.js
@@ -3188,19 +3188,14 @@ class ResultsView extends (0, _viewJsDefault.default) {
     _generateMarkupPreview(result) {
         return `
     <li class="preview">
-            <a class="preview__link preview__link--active" href="#${result.id}">
+            <a class="preview__link" href="#${result.id}">
               <figure class="preview__fig">
                 <img src="${result.image}" alt="${result.title}" />
               </figure>
               <div class="preview__data">
                 <h4 class="preview__title">${result.title}</h4>
                 <p class="preview__publisher">${result.publisher}</p>
-                <div class="preview__user-generated">
-                  <svg>
-                    <use href="${0, _iconsSvgDefault.default}#icon-user"></use>
-                  </svg>
-                </div>
-              </div>
+               
             </a>
           </li>
           `;
