210bc9979930a1ab67412c128580d07ed37b65e0
Yiranubari <yiranubari4@gmail.com>
2025-12-10 08:00:15 +0100

    Add PaginationView and integrate pagination in search results rendering

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -2,4 +2,13 @@ import View from './view.js';
 import icons from 'url:../../img/icons.svg';
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {
+    // Page 1, and there are other pages
+    // Page 1, and there are no other pages
+    // Last page
+    // Other page
+  }
 }
+
+export default new PaginationView();
