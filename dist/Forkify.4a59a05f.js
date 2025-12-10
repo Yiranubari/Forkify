e38c2ceaeb20e716edd76eed76017e56ac638353
Yiranubari <yiranubari4@gmail.com>
2025-12-10 11:39:35 +0100

    Fix pagination logic and update search results rendering
    - Changed the number of search results displayed per page from 6 to 1 in controller.js.
    - Updated paginationView.js to correctly reference the current page variable and improve pagination button rendering for different page scenarios.

--- dist/Forkify.4a59a05f.js
+++ dist/Forkify.4a59a05f.js
@@ -752,7 +752,7 @@ const controlSearchResults = async function() {
         // 2) Load search results
         await _modelJs.loadSearchResults(query);
         // 3) Render results
-        (0, _resultsViewJsDefault.default).render(_modelJs.getSearchResultsPage(6));
+        (0, _resultsViewJsDefault.default).render(_modelJs.getSearchResultsPage(1));
         // 4) Render initial pagination buttons
         (0, _paginationViewJsDefault.default).render(_modelJs.state.search);
     } catch (err) {
@@ -3227,14 +3227,21 @@ var _iconsSvgDefault = parcelHelpers.interopDefault(_iconsSvg);
 class PaginationView extends (0, _viewJsDefault.default) {
     _parentElement = document.querySelector('.pagination');
     _generateMarkup() {
+        const curPage = curPage;
         const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
         console.log(numPages);
         // Page 1, and there are other pages
-        if (this._data.page === 1 && numPages > 1) return 'page 1, others';
+        if (curPage === 1 && numPages > 1) return 'page 1, others';
         // Last page
-        if (this._data.page === numPages && numPages > 1) return 'last page';
+        if (curPage === numPages && numPages > 1) return `
+      <button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${0, _iconsSvgDefault.default}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+          </button>`;
         // Other page
-        if (this._data.page < numPages) return 'other page';
+        if (curPage < numPages) return 'other page';
         // Page 1, and there are no other pages
         return 'only 1 page';
     }
--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -4,21 +4,28 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkup() {
+    const curPage = curPage;
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
     console.log(numPages);
     // Page 1, and there are other pages
-    if (this._data.page === 1 && numPages > 1) {
+    if (curPage === 1 && numPages > 1) {
       return 'page 1, others';
     }
 
     // Last page
-    if (this._data.page === numPages && numPages > 1) {
-      return 'last page';
+    if (curPage === numPages && numPages > 1) {
+      return `
+      <button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+          </button>`;
     }
     // Other page
-    if (this._data.page < numPages) {
+    if (curPage < numPages) {
       return 'other page';
     }
     // Page 1, and there are no other pages
