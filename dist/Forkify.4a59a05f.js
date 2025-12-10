beb007c5ee7f415cdf6040322986d222c21cab65
Yiranubari <yiranubari4@gmail.com>
2025-12-10 11:44:49 +0100

    Enhance pagination buttons in PaginationView
    - Updated the rendering logic for pagination buttons.
    - Added a "Next" button for navigating to the next page when on the first page.
    - Added "Previous" and "Next" buttons for navigating between pages when not on the first or last page.
    - Removed unnecessary text for cases with only one page.

--- dist/Forkify.4a59a05f.js
+++ dist/Forkify.4a59a05f.js
@@ -3231,7 +3231,13 @@ class PaginationView extends (0, _viewJsDefault.default) {
         const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
         console.log(numPages);
         // Page 1, and there are other pages
-        if (curPage === 1 && numPages > 1) return 'page 1, others';
+        if (curPage === 1 && numPages > 1) return `
+      <button class="btn--inline pagination__btn--next">
+            <span>Page ${curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${0, _iconsSvgDefault.default}#icon-arrow-right"></use>
+            </svg>
+          </button>`;
         // Last page
         if (curPage === numPages && numPages > 1) return `
       <button class="btn--inline pagination__btn--prev">
@@ -3241,9 +3247,20 @@ class PaginationView extends (0, _viewJsDefault.default) {
             <span>Page ${curPage - 1}</span>
           </button>`;
         // Other page
-        if (curPage < numPages) return 'other page';
+        if (curPage < numPages) return `<button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${0, _iconsSvgDefault.default}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+          </button>
+          <button class="btn--inline pagination__btn--next">
+            <span>Page ${curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${0, _iconsSvgDefault.default}#icon-arrow-right"></use>
+            </svg>
+          </button>`;
         // Page 1, and there are no other pages
-        return 'only 1 page';
+        return '';
     }
 }
 exports.default = new PaginationView();
