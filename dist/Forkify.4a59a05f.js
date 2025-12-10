b1ca29b8ff2f09170feb0898451fba4b45df40b8
Yiranubari <yiranubari4@gmail.com>
2025-12-10 10:54:36 +0100

    Update search results pagination and enhance pagination view messages
    - Changed the initial page of search results from 1 to 6 in controller.js to display a different set of results.
    - Enhanced paginationView.js to provide clearer messages based on the current page:
      - Added conditions to indicate if it's the first page with other pages, the last page, any other page, or if there's only one page.

--- dist/Forkify.4a59a05f.js
+++ dist/Forkify.4a59a05f.js
@@ -752,7 +752,7 @@ const controlSearchResults = async function() {
         // 2) Load search results
         await _modelJs.loadSearchResults(query);
         // 3) Render results
-        (0, _resultsViewJsDefault.default).render(_modelJs.getSearchResultsPage(1));
+        (0, _resultsViewJsDefault.default).render(_modelJs.getSearchResultsPage(6));
         // 4) Render initial pagination buttons
         (0, _paginationViewJsDefault.default).render(_modelJs.state.search);
     } catch (err) {
@@ -3229,10 +3229,14 @@ class PaginationView extends (0, _viewJsDefault.default) {
     _generateMarkup() {
         const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
         console.log(numPages);
-    // Page 1, and there are other pages
-    // Page 1, and there are no other pages
-    // Last page
-    // Other page
+        // Page 1, and there are other pages
+        if (this._data.page === 1 && numPages > 1) return 'page 1, others';
+        // Last page
+        if (this._data.page === numPages && numPages > 1) return 'last page';
+        // Other page
+        if (this._data.page < numPages) return 'other page';
+        // Page 1, and there are no other pages
+        return 'only 1 page';
     }
 }
 exports.default = new PaginationView();
--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -9,9 +9,20 @@ class PaginationView extends View {
     );
     console.log(numPages);
     // Page 1, and there are other pages
-    // Page 1, and there are no other pages
+    if (this._data.page === 1 && numPages > 1) {
+      return 'page 1, others';
+    }
+
     // Last page
+    if (this._data.page === numPages && numPages > 1) {
+      return 'last page';
+    }
     // Other page
+    if (this._data.page < numPages) {
+      return 'other page';
+    }
+    // Page 1, and there are no other pages
+    return 'only 1 page';
   }
 }
 
