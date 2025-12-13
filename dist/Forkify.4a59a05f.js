81e083eca0278c158a26e3e80adb0025dbf569c1
Yiranubari <yiranubari4@gmail.com>
2025-12-13 11:52:34 +0100

    Reset search results page to 1 after loading new search results

--- dist/Forkify.4a59a05f.js
+++ dist/Forkify.4a59a05f.js
@@ -2088,7 +2088,7 @@ const loadSearchResults = async function(query) {
                 image: rec.image_url
             };
         });
-        console.log(state.search.results);
+        state.search.page = 1;
     } catch (err) {
         console.error(`${err}\u{1F525}\u{1F525}\u{1F525}`);
         throw err;
--- src/js/model.js
+++ src/js/model.js
@@ -47,7 +47,7 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
-    console.log(state.search.results);
+    state.search.page = 1;
   } catch (err) {
     console.error(`${err}🔥🔥🔥`);
     throw err;
