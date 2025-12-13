7acb0e968f0d41c39edfd00a5a79ec898f46b339
Yiranubari <yiranubari4@gmail.com>
2025-12-13 11:27:26 +0100

    Enhance view update logic to handle attribute changes in addition to text updates

--- dist/Forkify.4a59a05f.js
+++ dist/Forkify.4a59a05f.js
@@ -2901,7 +2901,10 @@ class View {
         const curElements = Array.from(this._parentElement.querySelectorAll('*'));
         newElements.forEach((newEl, i)=>{
             const curEl = curElements[i];
+            // Updates changed TEXT
             if (!newEl.isEqualNode(curEl) && newEl.firstChild?.nodeValue.trim() !== '') curEl.textContent = newEl.textContent;
+            // Updates changed ATTRIBUTES
+            if (!newEl.isEqualNode(curEl)) Array.from(newEl.attributes).forEach((attr)=>curEl.setAttribute(attr.name, attr.value));
         });
     }
     _clear() {
