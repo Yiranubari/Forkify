4a72552275cedaaa316b57abeabba54c3f57c679
Yiranubari <yiranubari4@gmail.com>
2025-12-16 03:10:24 +0100

    Implement AddRecipeView class with functionality to show and hide the recipe upload window

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,7 +1,19 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
 class AddRecipeView extends View {
-  _parentElement = document.querySelector('.add-recipe');
+  _parentElement = document.querySelector('.upload');
+
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', function () {
+      this._overlay.classList.toggle('hidden');
+      this._window.classList.toggle('hidden');
+    });
+  }
 
   _generateMarkup() {}
 }
