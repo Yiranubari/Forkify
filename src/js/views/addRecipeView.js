e777bb5b0099980dee6f26aea61a9c40ff4f9d12
Yiranubari <yiranubari4@gmail.com>
2025-12-16 03:06:06 +0100

    Add AddRecipeView class to handle recipe addition functionality

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,9 @@
+import View from './view.js';
+import icons from 'url:../../img/icons.svg';
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.add-recipe');
+
+  _generateMarkup() {}
+}
+
+export default new AddRecipeView();
