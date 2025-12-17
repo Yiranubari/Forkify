f03b4629752ff43bc18e8b2ff48dea15e335c891
Yiranubari <yiranubari4@gmail.com>
2025-12-17 03:56:29 +0100

    Add user-generated content indicator to recipe preview

--- dist/Forkify.4a59a05f.js
+++ dist/Forkify.4a59a05f.js
@@ -3436,6 +3436,12 @@ class PreviewView extends (0, _viewJsDefault.default) {
                 <h4 class="preview__title">${this._data.title}</h4>
                 <p class="preview__publisher">${this._data.publisher}</p>
               </div>
+              <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">
+              
+                        <svg>
+                            <use href="${0, _iconsSvgDefault.default}#icon-user"></use>
+                        </svg>
+                        </div>
             </a>
           </li>
           `;
